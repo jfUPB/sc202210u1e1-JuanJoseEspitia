26c34b1c355d8cbc963089c8dbe2784dc0649fa0
Juan José <juanjoseespitiaggmail.com>
2022-02-09 08:56:51 -0500

    programa modificado

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,6 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// Juan José Espitia Gómez
+// 000419497
+// juan.espitia@upb.edu.co
 (START)
     @START
     0;JMP
